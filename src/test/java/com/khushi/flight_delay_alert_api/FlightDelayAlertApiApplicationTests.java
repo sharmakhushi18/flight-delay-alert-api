79c496a5b80ee4973d0d517c674fa5fe90eeadf2
Khushi Sharma <sharmakhushi99631@gmail.com>
2026-04-08 05:45:16 +0530

    Final JWT project

---
+++ src/test/java/com/khushi/flight_delay_alert_api/FlightDelayAlertApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khushi.flight_delay_alert_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightDelayAlertApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
