6efda97d3b8698e1139f0818a457ed4ad21c32f7
Khushi Sharma <sharmakhushi99631@gmail.com>
2026-04-19 20:44:23 +0530

    Fix CORS headers

--- src/main/java/config/CorsConfig.java
+++ src/main/java/config/CorsConfig.java
@@ -10,11 +10,12 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-.allowedOrigins(
-    "http://localhost:3000",
-    "https://flight-delay-frontend-seven.vercel.app"
-)
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*");
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "https://flight-delay-frontend-seven.vercel.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .exposedHeaders("Authorization");
     }
 }
